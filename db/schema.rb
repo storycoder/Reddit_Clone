2c1b9f5fc2c44a086855d06424519b812a7bff18
Storycoder <joseph.margolis@gmail.com>
2015-10-08 10:27:40 -0400

    forgot to migrate user_id column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008140231) do
+ActiveRecord::Schema.define(version: 20151008142216) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20151008140231) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
